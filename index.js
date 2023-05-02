6a63587deadb15067aef0218a9d6fd28b49e8634
Masum <masumt999.com>
2023-05-02 20:27:03 +0600

    recipe data created

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const port = process.env.PORT || 5000; 
 
 const chefs = require('./data/chefs.json');
+const recipe = require('./data/recipe.json');
 
 app.use(cors());
 
@@ -17,6 +18,25 @@ app.get('/chefs', (req, res) => {
     res.send(chefs);
 })
 
+app.get('/recipe', (req, res) => {
+    res.send(recipe)
+})
+
+// app.get('/recipe/:id', (req, res) => {
+//     const id = req.params.id;
+//     // console.log(id);
+//     const selectedRecipe = recipe.find(n => n._id === id);
+//     res.send(selectedRecipe)
+// })
+
+
+// app.get('/chefs/:id', (req, res) => {
+//     const id = parseInt(req.params.id);
+//     // console.log(id);
+//     const chefRecipe = recipe.filter(n => parseInt(n.chef_id) === id);
+//     res.send(chefRecipe)
+
+// })
 
 app.listen(port, () => {
     console.log(`Recipe API is running on port ${port}`)
